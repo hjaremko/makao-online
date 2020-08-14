758aea4242ea9b1c05cb1027da137caeea6a73ad
Hubert Jaremko <hjaremko@outlook.com>
2020-08-14 20:17:16 +0200

    Refactor network module some more

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,13 +1,11 @@
 mod network;
 
-use log::{debug, error, info, LevelFilter};
-use network::handle_client;
+use crate::network::{handle_connections, Broadcaster};
+use log::{info, LevelFilter};
+use std::env;
 use std::io::Error;
-use std::{env, thread, time};
-use tokio::io::ErrorKind;
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::TcpListener;
 use tokio::sync::broadcast;
-use tokio::sync::broadcast::Sender;
 
 type DataPacket = i32;
 type BoxError = Box<dyn std::error::Error>;
@@ -17,6 +15,7 @@ type IoResult = Result<(), Error>;
 #[tokio::main]
 pub async fn main() -> BoxResult {
     logger::init(LevelFilter::max()).unwrap();
+    info!("Makao Online Server");
 
     let server_addr = get_server_addr();
     let mut listener = TcpListener::bind(&server_addr).await?;
@@ -33,58 +32,4 @@ fn get_server_addr() -> String {
     env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string())
-}
-
-async fn handle_connections(listener: &mut TcpListener, tx2: Sender<i32>) -> BoxResult {
-    loop {
-        let (socket, client_addr) = listener.accept().await?;
-        info!("[{:?}] connected!", client_addr);
-
-        spawn_client_handler(socket, tx2.clone());
-    }
-}
-
-fn spawn_client_handler(socket: TcpStream, tx: Sender<DataPacket>) {
-    tokio::spawn(async move {
-        let rx = tx.subscribe();
-        if let Err(e) = handle_client(socket, rx).await {
-            error!("client thread failed with error: {}", e);
-        }
-    });
-}
-
-struct Broadcaster {
-    tx: Sender<DataPacket>,
-}
-
-impl Broadcaster {
-    pub fn new(tx: Sender<i32>) -> Self {
-        Broadcaster { tx }
-    }
-
-    pub fn spawn(self) {
-        debug!("spawning broadcast thread");
-        tokio::spawn(async move {
-            if let Err(e) = self.broadcast().await {
-                error!("broadcast thread failed with error: {}", e);
-            }
-        });
-    }
-
-    async fn broadcast(self) -> IoResult {
-        let mut i = 0;
-        loop {
-            if self.send_to_channel(i).is_ok() {
-                i += 1;
-            }
-            thread::sleep(time::Duration::from_millis(1000));
-        }
-    }
-
-    fn send_to_channel(&self, i: DataPacket) -> IoResult {
-        if let Err(e) = self.tx.send(i) {
-            return Err(Error::new(ErrorKind::Other, format!("{:?}", e)));
-        }
-        Ok(())
-    }
 }
--- src/bin/server/network.rs
+++ src/bin/server/network.rs
@@ -1,106 +1,209 @@
-use crate::{DataPacket, IoResult};
-use log::{debug, error};
+use crate::{BoxResult, DataPacket, IoResult};
+use log::{debug, error, info};
 use std::io::{Error, ErrorKind};
 use std::net::SocketAddr;
-use std::str;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
+use std::{str, thread, time};
+use tokio::io::AsyncWriteExt;
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
-use tokio::net::TcpStream;
-use tokio::prelude::*;
-use tokio::sync::broadcast::{Receiver, TryRecvError};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::prelude::io::*;
+use tokio::sync::broadcast::{Receiver, Sender, TryRecvError};
 
-pub async fn handle_client(stream: TcpStream, rx: Receiver<DataPacket>) -> IoResult {
-    let mut buf = [0; 1024];
-    let client_addr = stream.peer_addr()?;
-
-    debug!("processing {}", client_addr);
-
-    let (mut read_str, write_str) = stream.into_split();
-    let not_closed = Arc::new(AtomicBool::new(true));
-    let rec_closed = not_closed.clone();
-
-    spawn_sender(write_str, rx, rec_closed);
-    let res = receive_from_client(&mut buf, client_addr, &mut read_str).await;
-
-    not_closed.swap(false, Ordering::Relaxed);
-    debug!("connection closed");
-    res
-}
-
-async fn receive_from_client(
-    mut buf: &mut [u8],
-    client_addr: SocketAddr,
-    read_str: &mut OwnedReadHalf,
-) -> IoResult {
+pub async fn handle_connections(listener: &mut TcpListener, tx2: Sender<DataPacket>) -> BoxResult {
     loop {
-        // TODO: close gracefully and return Ok(())
-        let n = read_str.read(&mut buf).await?;
-
-        if n == 0 {
-            break Err(Error::new(ErrorKind::InvalidData, "read 0 bytes"));
-        }
-
-        debug!("[{}] read {} bytes", client_addr, n);
-
-        let mut buf: String = match str::from_utf8(&buf[0..n]) {
-            Ok(str) => str.to_string(),
-            Err(e) => {
-                break Err(Error::new(ErrorKind::InvalidInput, e));
-            }
-        };
-
-        buf.retain(|c| c.is_alphanumeric());
-        debug!("got {}", buf);
+        let (socket, client_addr) = listener.accept().await?;
+        info!("[{}] connected!", client_addr);
+        ClientConnection::new(socket, tx2.subscribe()).spawn();
     }
 }
 
-fn spawn_sender(write_str: OwnedWriteHalf, rx: Receiver<DataPacket>, rec_closed: Arc<AtomicBool>) {
-    tokio::spawn(async move {
-        if let Err(e) = forward_broadcasts(rx, write_str, rec_closed).await {
-            error!("sender thread failed with error: {}", e);
-        }
-    });
+pub struct Broadcaster {
+    tx: Sender<DataPacket>,
 }
 
-async fn forward_broadcasts(
-    mut rx: Receiver<DataPacket>,
-    mut write_stream: OwnedWriteHalf,
+impl Broadcaster {
+    pub fn new(tx: Sender<i32>) -> Self {
+        Broadcaster { tx }
+    }
+
+    pub fn spawn(self) {
+        debug!("spawning broadcast thread");
+        tokio::spawn(async move {
+            if let Err(e) = self.broadcast() {
+                error!("broadcast thread failed with error: {}", e);
+            }
+            debug!("broadcast thread ended");
+        });
+    }
+
+    fn broadcast(self) -> IoResult {
+        let mut i = 0;
+        loop {
+            if self.send_to_channel(i).is_ok() {
+                i += 1;
+            }
+            thread::sleep(time::Duration::from_millis(1000));
+        }
+    }
+
+    fn send_to_channel(&self, data: DataPacket) -> IoResult {
+        if let Err(e) = self.tx.send(data) {
+            return Err(Error::new(ErrorKind::Other, format!("{:?}", e)));
+        }
+        Ok(())
+    }
+}
+
+pub struct ClientConnection {
+    stream: TcpStream,
+    address: SocketAddr,
+    rx: Receiver<DataPacket>,
+    connected: Arc<AtomicBool>,
+}
+
+impl ClientConnection {
+    pub fn new(stream: TcpStream, rx: Receiver<DataPacket>) -> Self {
+        let addr = stream.peer_addr().unwrap();
+        ClientConnection {
+            stream,
+            address: addr,
+            rx,
+            connected: Arc::new(AtomicBool::new(true)),
+        }
+    }
+
+    pub fn spawn(self) {
+        tokio::spawn(async move {
+            if let Err(e) = self.handle_client() {
+                error!("client thread failed with error: {}", e);
+            }
+        });
+    }
+
+    fn handle_client(self) -> IoResult {
+        debug!("processing {}", self.address);
+
+        let (read_str, write_str) = self.stream.into_split();
+        ClientSender::new(write_str, self.rx, self.connected.clone()).spawn();
+        ClientReceiver::new(read_str, self.connected).spawn();
+
+        Ok(())
+    }
+}
+
+struct ClientSender {
+    write_stream: OwnedWriteHalf,
+    rx: Receiver<DataPacket>,
     not_closed: Arc<AtomicBool>,
-) -> IoResult {
-    let res: IoResult = loop {
-        if !not_closed.load(Ordering::Relaxed) {
-            break Ok(());
-        }
-
-        try_forward_broadcast(&mut write_stream, &mut rx).await?;
-    };
-
-    debug!("ending sending broadcast to client");
-    res
 }
 
-async fn try_forward_broadcast(
-    write_stream: &mut OwnedWriteHalf,
-    rx: &mut Receiver<DataPacket>,
-) -> IoResult {
-    match rx.try_recv() {
-        Ok(message) => send_to_client(write_stream, message).await,
-        Err(e) => match e {
-            TryRecvError::Empty => Ok(()),
-            TryRecvError::Closed => Err(Error::new(ErrorKind::NotConnected, "channel closed")),
-            TryRecvError::Lagged(_) => {
-                debug!("broadcast lagged");
-                rx.try_recv().unwrap();
-                Ok(())
+impl ClientSender {
+    pub fn new(
+        write_stream: OwnedWriteHalf,
+        rx: Receiver<DataPacket>,
+        not_closed: Arc<AtomicBool>,
+    ) -> Self {
+        ClientSender {
+            write_stream,
+            rx,
+            not_closed,
+        }
+    }
+
+    pub fn spawn(self) {
+        debug!("spawning sender thread");
+        tokio::spawn(async move {
+            if let Err(e) = self.forward_broadcasts().await {
+                error!("sender thread failed with error: {}", e);
             }
-        },
+        });
+    }
+
+    async fn forward_broadcasts(mut self) -> IoResult {
+        let res: IoResult = loop {
+            if !self.not_closed.load(Ordering::Relaxed) {
+                break Ok(());
+            }
+
+            self.try_forward_broadcast().await?;
+        };
+        res
+    }
+
+    async fn try_forward_broadcast(&mut self) -> IoResult {
+        match self.rx.try_recv() {
+            Ok(message) => self.send(message).await,
+            Err(e) => match e {
+                TryRecvError::Empty => Ok(()),
+                TryRecvError::Closed => Err(Error::new(ErrorKind::NotConnected, "channel closed")),
+                TryRecvError::Lagged(_) => {
+                    debug!("broadcast lagged");
+                    self.rx.try_recv().unwrap();
+                    Ok(())
+                }
+            },
+        }
+    }
+
+    async fn send(&mut self, message: DataPacket) -> IoResult {
+        debug!("sending to client: {}", message);
+        self.write_stream
+            .write_all(format!("{}\n", message).as_ref())
+            .await
     }
 }
 
-async fn send_to_client(write_stream: &mut OwnedWriteHalf, message: DataPacket) -> IoResult {
-    debug!("sending to client: {}", message);
-    write_stream
-        .write_all(format!("{}\n", message).as_ref())
-        .await
+struct ClientReceiver {
+    read_stream: OwnedReadHalf,
+    not_closed: Arc<AtomicBool>,
+}
+
+impl ClientReceiver {
+    pub fn new(read_stream: OwnedReadHalf, not_closed: Arc<AtomicBool>) -> Self {
+        ClientReceiver {
+            read_stream,
+            not_closed,
+        }
+    }
+
+    pub fn spawn(mut self) {
+        debug!("spawning receiver thread");
+        tokio::spawn(async move {
+            if let Err(e) = self.receive().await {
+                error!("receiver thread failed with error: {}", e);
+                debug!("setting connection indicator to false");
+                self.not_closed.swap(false, Ordering::Relaxed);
+            }
+        });
+    }
+
+    async fn receive(&mut self) -> IoResult {
+        // todo: handle in logger instance
+        // let client_addr = self.read_stream;
+        let mut buffer = [0; 1024];
+
+        loop {
+            // todo: close gracefully and return Ok(())
+            debug!("waiting for data");
+            let n = self.read_stream.read(&mut buffer).await?;
+
+            if n == 0 {
+                break Err(Error::new(ErrorKind::InvalidData, "read 0 bytes"));
+            }
+
+            // trace!("[{}] read {} bytes", client_addr, n);
+
+            let mut buf: String = match str::from_utf8(&buffer[0..n]) {
+                Ok(str) => str.to_string(),
+                Err(e) => {
+                    break Err(Error::new(ErrorKind::InvalidInput, e));
+                }
+            };
+
+            buf.retain(|c| c.is_alphanumeric());
+            debug!("got \"{}\"", buf);
+        }
+    }
 }
