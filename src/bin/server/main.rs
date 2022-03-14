33995e7bcf9a7aa1b48db3d6e5b3589bbae246ad
Hubert Jaremko <hjaremko@outlook.com>
2022-03-14 08:49:03 +0000

    wip

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,35 +1,150 @@
-mod network;
-
-use crate::network::{handle_connections, Broadcaster};
+use std::collections::HashMap;
+use std::fs;
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc,
+};
+use serde_derive::{Deserialize, Serialize};
+use futures_util::{SinkExt, StreamExt, TryFutureExt};
 use log::{info, LevelFilter};
-use std::env;
-use std::io::Error;
-use tokio::net::TcpListener;
-use tokio::sync::broadcast;
+use tokio::sync::{mpsc, RwLock};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use warp::ws::{Message, WebSocket};
+use warp::{Filter, Reply};
 
-type DataPacket = i32;
-type BoxError = Box<dyn std::error::Error>;
-type BoxResult = Result<(), BoxError>;
-type IoResult = Result<(), Error>;
+/// Our global unique user id counter.
+static NEXT_USER_ID: AtomicUsize = AtomicUsize::new(1);
+
+/// Our state of currently connected users.
+///
+/// - Key is their id
+/// - Value is a sender of `warp::ws::Message`
+type Users = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
+
+#[derive(Serialize, Deserialize)]
+struct LoginResponse {
+    token: String,
+}
 
 #[tokio::main]
-pub async fn main() -> BoxResult {
+async fn main() {
     logger::init(LevelFilter::max()).unwrap();
     info!("Makao Online Server");
 
-    let server_addr = get_server_addr();
-    let mut listener = TcpListener::bind(&server_addr).await?;
-    info!("listening on {}", &server_addr);
+    // Keep track of all connected users, key is usize, value
+    // is a websocket sender.
+    let users = Users::default();
+    // Turn our "state" into a new Filter...
+    let users = warp::any().map(move || users.clone());
 
-    let (tx, _) = broadcast::channel(16);
-    let tx2 = tx.clone();
+    // GET /chat -> websocket upgrade
+    let join = warp::path("join")
+        // The `ws()` filter will prepare Websocket handshake...
+        .and(warp::ws())
+        .and(users)
+        .map(|ws: warp::ws::Ws, users| {
+            // This will call our function if the handshake succeeds.
+            ws.on_upgrade(move |socket| user_connected(socket, users))
+        });
 
-    Broadcaster::new(tx).spawn();
-    handle_connections(&mut listener, tx2).await
+    // GET / -> index html
+    let index = warp::path::end().map(|| {
+        let p = std::env::current_dir().unwrap();
+        // let p = p.to_str().unwrap();
+        // info!("{}", p);
+        let index_html = fs::read_to_string(p.join("src/bin/server/static/index.html")).unwrap();
+        warp::reply::html(index_html)
+    });
+    let login = warp::post()
+        .and(warp::path("login"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::param::<String>())
+        .map(|username, password| {
+            info!("Logged in: {}/{}", username, password);
+            let r = LoginResponse { token: "1234".to_string() };
+            warp::reply::json(&r)
+        });
+
+    let routes = index.or(join).or(login);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
 
-fn get_server_addr() -> String {
-    env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string())
+async fn user_connected(ws: WebSocket, users: Users) {
+    // Use a counter to assign a new unique ID for this user.
+    let my_id = NEXT_USER_ID.fetch_add(1, Ordering::Relaxed);
+
+    eprintln!("new chat user: {}", my_id);
+
+    // Split the socket into a sender and receive of messages.
+    let (mut user_ws_tx, mut user_ws_rx) = ws.split();
+
+    // Use an unbounded channel to handle buffering and flushing of messages
+    // to the websocket...
+    let (tx, rx) = mpsc::unbounded_channel();
+    let mut rx = UnboundedReceiverStream::new(rx);
+
+    tokio::task::spawn(async move {
+        while let Some(message) = rx.next().await {
+            user_ws_tx
+                .send(message)
+                .unwrap_or_else(|e| {
+                    eprintln!("websocket send error: {}", e);
+                })
+                .await;
+        }
+    });
+
+    // Save the sender in our list of connected users.
+    users.write().await.insert(my_id, tx);
+
+    // Return a `Future` that is basically a state machine managing
+    // this specific user's connection.
+
+    // Every time the user sends a message, broadcast it to
+    // all other users...
+    while let Some(result) = user_ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                eprintln!("websocket error(uid={}): {}", my_id, e);
+                break;
+            }
+        };
+        user_message(my_id, msg, &users).await;
+    }
+
+    // user_ws_rx stream will keep processing as long as the user stays
+    // connected. Once they disconnect, then...
+    user_disconnected(my_id, &users).await;
+}
+
+async fn user_message(my_id: usize, msg: Message, users: &Users) {
+    // Skip any non-Text messages...
+    let msg = if let Ok(s) = msg.to_str() {
+        s
+    } else {
+        return;
+    };
+
+    let new_msg = format!("<User#{}>: {}", my_id, msg);
+    info!("{}", new_msg);
+
+    // New message from this user, send it to everyone else (except same uid)...
+    for (&uid, tx) in users.read().await.iter() {
+        if my_id != uid {
+            if let Err(_disconnected) = tx.send(Message::text(new_msg.clone())) {
+                // The tx is disconnected, our `user_disconnected` code
+                // should be happening in another task, nothing more to
+                // do here.
+            }
+        }
+    }
+}
+
+async fn user_disconnected(my_id: usize, users: &Users) {
+    eprintln!("good bye user: {}", my_id);
+
+    // Stream closed up, so remove from the user list
+    users.write().await.remove(&my_id);
 }
